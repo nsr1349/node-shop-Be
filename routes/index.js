d27539b989111d0b88d0bcde1905f04bfe343e92
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-30 00:25:15 +0900

    feat: 라우터체 product 추가

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const userApi = require('./user.api')
+const productApi = require('./product.api')
 
 router.use('/user', userApi)
+
+router.use('/product', productApi)
 
 module.exports = router
