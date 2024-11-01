4874a57d92d1b08f576e667754d4967e2df6256c
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-11-02 00:46:07 +0900

    feat: 삭제 및 수정 기능 추가

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -16,8 +16,9 @@ productController.createProduct = async (req, res) => {
 productController.getProduct = async (req, res) => {
     try {
         const { page, q, size, active } = req.query
-        const cond = q ? {name:{$regex:q, $options:'i'}} : {}
-        if (active === 'true') cond.status = 'active';
+        const cond = q ? {name:{$regex:q, $options:'i'}, isDeleted : false} : {}
+        cond.isDeleted = false
+        if (active === 'true') cond.status = 'active'
         
         let query = Product.find(cond).sort({ createdAt: -1 })
         let response = {status : 'success' }
@@ -34,6 +35,38 @@ productController.getProduct = async (req, res) => {
         res.status(200).json(response)
     } catch ({message}) {
         res.status(400).json({status : 'fail', message})
+    }
+}
+
+productController.updateProduct = async (req, res) => {
+    try {
+        const { sku , name, size, image, category, description, price, stock, status} = req.body 
+        const product = await Product.findByIdAndUpdate(
+            {_id : req.params.id }, 
+            { sku , name, size, image, category, description, price, stock, status},
+            { new : true}, 
+        )
+        if (!product) throw new Error('수정하려는 상품을 찾을 수 없습니다')
+
+        res.status(200).json({ status : 'success', product })
+    } catch ({message}) {
+        res.status(400).json({ status : 'fail', message })
+    }
+}
+
+productController.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(
+            {_id : req.params.id }, 
+            { isDeleted : true },
+            { new : true}, 
+        )
+        console.log(req.params.id)
+        if (!product) throw new Error('삭제하려는 상품을 찾을 수 없습니다')
+
+        res.status(200).json({ status : 'success', product })
+    } catch ({message}) {
+        res.status(400).json({ status : 'fail', message })
     }
 }
 
--- routes/product.api.js
+++ routes/product.api.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { authenticate,checkAdminPermission } = require('../controller/auth.controller')
-const { createProduct, getProduct } = require('../controller/product.controller')
+const { createProduct, getProduct, updateProduct, deleteProduct } = require('../controller/product.controller')
 
 router.post('/', authenticate, checkAdminPermission, createProduct)
+
+router.put('/:id', authenticate, checkAdminPermission, updateProduct)
+
+router.delete('/:id', authenticate, checkAdminPermission, deleteProduct)
 
 router.get('/', getProduct)
 
