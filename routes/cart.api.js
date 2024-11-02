98b514f8920c662487c9786a12f6e50e2b844d63
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-11-03 03:15:50 +0900

    feat: getCart 구현 2

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { authenticate } = require('../controller/auth.controller')
-const { addCart } = require('../controller/cart.controller')
+const { addCart, getCart } = require('../controller/cart.controller')
 
 router.post('/', authenticate, addCart)
+
+router.get('/', authenticate, getCart)
 
 module.exports = router
