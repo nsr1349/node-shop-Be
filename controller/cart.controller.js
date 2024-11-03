063e868fbb69d48b2801b922d1bf97815b4cc372
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-11-04 02:08:57 +0900

    feat: 백엔드 요구사항완료

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -31,12 +31,54 @@ cartController.addCart = async (req, res) => {
 cartController.getCart = async (req, res) => {
     try {
         const { userId } = req
-        console.log(userId)
-        let cart = await Cart.findOne({ userId })
+        const cart = await Cart.findOne({ userId }).populate({
+            path : "items",
+            populate : {
+                path : "productId",
+                model : 'Product'
+            }
+        })
         res.status(200).json({status : 'success', cart})
     } catch ({message}) {
         res.status(400).json({status : 'fail', message})
     }
 }
+
+cartController.editCart = async (req, res) => {
+    try {
+        const { userId } = req
+        const { items } = req.body
+
+        const cart = await Cart.findOneAndUpdate(
+            { userId }, 
+            { items },
+        ).populate({
+            path : "items",
+            populate : {
+                path : "productId",
+                model : 'Product'
+            }
+        })
+
+        await cart.save();
+        res.status(200).json({status : 'success', cart})
+    } catch ({message}) {
+        res.status(400).json({status : 'fail', message})
+    }
+}
+
+cartController.deleteCart = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req;
+        const cart = await Cart.findOne({ userId });
+        cart.items = cart.items.filter((item) => !item._id.equals(id));
+
+        await cart.save();
+        res.status(200).json({status : 'success', cart})
+    } catch (error) {
+        res.status(400).json({status : 'fail', message})
+    }
+};
 
 module.exports = cartController
--- routes/cart.api.js
+++ routes/cart.api.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const { authenticate } = require('../controller/auth.controller')
-const { addCart, getCart } = require('../controller/cart.controller')
+const { addCart, getCart, editCart, deleteCart } = require('../controller/cart.controller')
 
 router.post('/', authenticate, addCart)
 
 router.get('/', authenticate, getCart)
+
+router.put('/', authenticate, editCart)
+
+router.delete('/:id', authenticate, deleteCart)
 
 module.exports = router
