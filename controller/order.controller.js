ac7fc88f4655d4ff9a390a583e64623b33eea631
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-11-09 01:47:46 +0900

    feat: 요구사항 구현

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -1,7 +1,6 @@
 const Order = require('../model/Order')
 const { randomStringGenerator } = require('../utils/randomStringGenerator')
 const { checkItemListStock } = require('./product.controller')
-const { editCart } = require('./cart.controller')
 
 const orderController = {}
 
@@ -29,6 +28,72 @@ orderController.createOrder = async (req, res) => {
         await newOrder.save()
 
         res.status(200).json({status : 'success', orderNum: newOrder.orderNum })
+    } catch ({message}) {
+        res.status(400).json({status : 'fail', message})
+    }
+}
+
+orderController.getOrder = async (req, res) => {
+    try {
+        const { userId } = req 
+        const { page, q, size, isMyOrder } = req.query
+
+        const cond = q ? {orderNum:{$regex:q, $options:'i'}} : {}
+        if (isMyOrder) cond.userId = userId
+
+        let query = Order.find(cond)
+                            .populate({
+                                path : "items",
+                                populate : {
+                                    path : "productId",
+                                    model : 'Product'
+                                }
+                            })
+                            .sort({ createdAt: -1 })
+        let response = {status : 'success' }
+            
+        if(page){
+            query.skip((page-1)*size).limit(size)
+            const totalItemNum = await Order.countDocuments(cond);
+            response.totalPageNum =  Math.ceil(totalItemNum / size)
+        }
+    
+        const orders = await query.exec()
+        response.orders = orders
+
+        res.status(200).json(response)
+    } catch ({message}) {
+        res.status(400).json({status : 'fail', message})
+    }
+}
+
+orderController.getSingleOrder = async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id)
+                                    .populate({
+                                        path : "items",
+                                        populate : {
+                                            path : "productId",
+                                            model : 'Product',
+                                        }
+                                    })
+                                    .populate('userId' , 'name email', 'User')
+
+        res.status(200).json({status : 'success', order})
+    } catch ({message}) {
+        res.status(400).json({status : 'fail', message})
+    }
+}
+
+orderController.updateOrder = async (req, res) => {
+    try {
+        const { status } = req.body 
+        const order = await Order.findByIdAndUpdate(
+            {_id : req.params.id }, 
+            { status },
+            { new : true}, 
+        )
+        res.status(200).json({status : 'success', order})
     } catch ({message}) {
         res.status(400).json({status : 'fail', message})
     }
--- routes/order.api.js
+++ routes/order.api.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const { authenticate } = require('../controller/auth.controller') 
-const { createOrder } = require('../controller/order.controller')
+const { authenticate,checkAdminPermission } = require('../controller/auth.controller') 
+const { createOrder, getOrder , getSingleOrder, updateOrder } = require('../controller/order.controller')
 
 router.post('/', authenticate, createOrder)
+
+router.get('/', authenticate, getOrder)
+
+router.get('/:id', authenticate, getSingleOrder)
+
+router.put('/:id', authenticate, checkAdminPermission, updateOrder)
 
 module.exports = router
